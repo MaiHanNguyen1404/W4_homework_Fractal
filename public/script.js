b983b1401de30f4283e1624af5c01c6baf96a0d4
Mai Hân <maihannguyen@Mai-Han.local>
2024-04-10 23:04:17 +1000

    fractal tree

--- public/script.js
+++ public/script.js
@@ -1,22 +1,50 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-const cnv = document.getElementById (`cnv_element`)
+const cnv = document.getElementById (`fractal`)
 cnv.width = innerWidth
 cnv.height = innerHeight
+document.body.appendChild (cnv)
 
 const ctx = cnv.getContext (`2d`)
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
+// set the fill to blue
+ctx.fillStyle = `blue`
+// fill the background
+ctx.fillRect (0, 0, cnv.width, cnv.height)
 
-   requestAnimationFrame (draw_frame)
+// set the stroke color
+ctx.strokeStyle = `yellow`
+ctx.lineWidth = 10
+
+// set the origin of the branch
+ctx.translate (cnv.width/2, cnv.height)
+
+
+function drawBranch (length) {
+   // draw the base
+   ctx.beginPath ()
+   ctx.moveTo (0, 0) 
+   ctx.lineTo (0, -length)
+   ctx.stroke ()
+
+   ctx.translate (0, -length)
+
+   // stop the excuting if the branch length is less than 10
+   if (length > 10) {
+    // draw right branch
+    ctx.save () 
+    ctx.rotate (0.5)
+    drawBranch (length * 0.75)
+    ctx.restore () //return previous setting
+
+    // draw left branch
+    ctx.save ()
+    ctx.rotate (-0.5)
+    drawBranch (length * 0.75)
+    ctx.restore () //return previous setting
+   } 
+
 }
 
-draw_frame ()
-
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+drawBranch (250)
